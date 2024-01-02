4fb46e2e8ebfadf83a00c1a0e9d3dab6f1cf218f
mukundmadhav-kalvium <mukund.madhav@kalvium.community>
2024-01-02 23:33:26 +0530

    Image Carousel Functional

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,42 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+// you can explore more - and check as how to use materiul ui
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+// implement the class below
+export default function Carousel() {
+  const [currIndex, setCurrIndex] = useState(0);
 
-// complete the function below:
-function Carousel() {
+  const increaseCount = () => {
+    if (currIndex >= images.length - 1) {
+      setCurrIndex(0);
+    } else {
+      setCurrIndex(currIndex + 1);
+    }
+  };
+  const decreaseCount = () => {
+    if (currIndex == 0) {
+      setCurrIndex(images.length - 1);
+    } else {
+      setCurrIndex((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Carousel</h1>
+      <div className="carousel-container flex">
+        <div className="leftArrow arrowDiv flex" onClick={decreaseCount}>
+          <ArrowBackIosIcon />
+        </div>
+        <h2 className="title">{images[currIndex].title}</h2>
+        <img src={images[currIndex].img} alt="" />
+        <h2 className="caption">{images[currIndex].subtitle}</h2>
+        <div className="rightArrow arrowDiv flex" onClick={increaseCount}>
+          <ArrowForwardIosIcon />
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default Carousel;
